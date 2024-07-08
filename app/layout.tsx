299ced8483adeb2ce436e81a40514fcc85c9d377
Dilshan <tharakadilshan506@gmail.com>
2024-07-09 00:18:37 +0530

    Change some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar/Navbar";
-import { Footer } from "./components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
